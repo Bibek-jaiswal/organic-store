9714e1c5f5c0c1bd90a6a98d102487bfeedde7f7
Bibek Jaiswal <bibek.jaiswal.codewing@gmail.com>
2023-01-17 17:06:58 +0545

    initial commit

---
+++ js/index.js
@@ -0,0 +1,20 @@
+function myFunction() {
+    var x = document.getElementById("test2");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
+
+
+
+
+function myFunction1() {
+    var x = document.getElementById("test2");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+}
